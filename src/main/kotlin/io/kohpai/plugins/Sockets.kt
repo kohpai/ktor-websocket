8f25975c16e36edc595c78d35e20c699eec01f92
Kohpai <whcwhc78@gmail.com>
2022-09-19 22:07:53 +0200

    :sparkles: broadcast message

--- src/main/kotlin/io/kohpai/plugins/Sockets.kt
+++ src/main/kotlin/io/kohpai/plugins/Sockets.kt
@@ -1,10 +1,12 @@
 package io.kohpai.plugins
 
+import io.kohpai.Connection
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import java.time.Duration
+import java.util.Collections
 
 fun Application.configureSockets() {
     install(WebSockets) {
@@ -15,12 +17,25 @@ fun Application.configureSockets() {
     }
 
     routing {
+        val connections =
+            Collections.synchronizedSet(LinkedHashSet<Connection?>())
         webSocket("/chat") { // websocketSession
-            send("You are connected!")
-            for (frame in incoming) {
-                frame as? Frame.Text ?: continue
-                val receiveText = frame.readText()
-                send("You said: $receiveText")
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receiveText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receiveText"
+                    connections.forEach { it!!.session.send(textWithUsername) }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing ${thisConnection.name}!")
+                connections -= thisConnection
             }
         }
     }
